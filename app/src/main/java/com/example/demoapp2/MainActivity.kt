9a68850f6709aa67747859c8f68d904cd95affff
Silvia <silvia@encapto.com>
2019-07-03 21:53:44 +1000

    new commit

--- app/src/main/java/com/example/demoapp2/MainActivity.kt
+++ app/src/main/java/com/example/demoapp2/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.demoapp2
 
+import android.content.Context
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -10,18 +12,24 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var mediaPlayer: MediaPlayer
+    lateinit var audioManager: AudioManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mediaPlayer = MediaPlayer.create(this, R.raw.mp3)
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        var curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+        var maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
         var volumeControl: SeekBar = findViewById(R.id.seekBar) as SeekBar
+        volumeControl.max = maxVolume
+        volumeControl.progress = curVolume
         volumeControl.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                Log.d("SeekBar value", progress.toString())
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0)
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-
+            fun iLoveKotlin(){}
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
     }
