298f7b5aa42501497fc44986cf05b74f6c397c99
Silvia <silvia@encapto.com>
2019-07-04 17:24:29 +1000

    new commit

--- app/src/main/java/com/example/demoapp2/MainActivity.kt
+++ app/src/main/java/com/example/demoapp2/MainActivity.kt
@@ -5,10 +5,14 @@ import android.media.AudioManager
 import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Timer
+import java.util.TimerTask
+import kotlin.concurrent.fixedRateTimer
 
 class MainActivity : AppCompatActivity() {
     lateinit var mediaPlayer: MediaPlayer
@@ -29,23 +33,19 @@ class MainActivity : AppCompatActivity() {
                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0)
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-            fun iLoveKotlin(){}
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
 
-//        var curProgress = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
-//        var maxProgress = audioManager.get(AudioManager.STREAM_MUSIC)
-//        var progressControl: SeekBar = findViewById(R.id.audioSeekBar) as SeekBar
-//        progressControl.max = mediaPlayer.duration
-//        volumeControl.progress = curVolume
-//        progressControl.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
-//            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-//                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0)
-//            }
-//            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-//            fun iLoveKotlin(){}
-//            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-//        })
+        var progressControl: SeekBar = findViewById(R.id.audioSeekBar) as SeekBar
+        progressControl.max = mediaPlayer.duration
+        fixedRateTimer("default", false, 0L, 1000){
+            progressControl.progress = mediaPlayer.currentPosition
+        }
+        progressControl.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {mediaPlayer.seekTo(progress)}
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {mediaPlayer?.pause()}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {mediaPlayer?.start()}
+        })
     }
     fun playAudio(view:View){
         mediaPlayer?.start()
